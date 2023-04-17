eef3588c4ebae8ec9d499086418ea351aff7453c
Yuki <910870146@qq.com>
2023-04-16 22:14:03 -0400

    change same day query

--- database_buildup.py
+++ database_buildup.py
@@ -122,7 +122,7 @@ print('helper table done')
 print('infotracer and sentiment table start')
 today = datetime.now(pytz.timezone('America/Mexico_City')).date().strftime('%Y-%m-%d')
 tomorrow=(datetime.now(pytz.timezone('America/Mexico_City')).date() + timedelta(days=1)).strftime('%Y-%m-%d')
-generate_infotracer_and_sentiment_table(start_date=today, end_date=today, ytb_end_date=tomorrow,query_dict=query_dict,update_db=True)
+generate_infotracer_and_sentiment_table(start_date=today, end_date=today, ytb_end_date=today,query_dict=query_dict,update_db=True)
 print('infotracer and sentiment table done')
 
 # wordcloud: daily update
