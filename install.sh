7001c4533401395db2b3719746da7b88d7dd6dd9
Yuki <910870146@qq.com>
2023-07-03 21:06:02 +0800

    installation

---
+++ install.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/zhouhanc/superset-information-tracer.git
+git fetch
+git checkout -b new_spanish origin/new_spanish
+
+
+pip install --upgrade pip
+pip install informationtracer
+pip install mysql-connector-python
+pip install pandas
+pip install numpy
+pip install pysentimiento
+pip install nltk
+pip install wordcloud
